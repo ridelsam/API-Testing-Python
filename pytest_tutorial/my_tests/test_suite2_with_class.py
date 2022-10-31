e7226236abc52714b05b609995904d69ae1df756
Ridel Samonte <ridel.asam@gmail.com>
2022-10-30 18:12:13 -0700

    testing pytest with function and class

---
+++ pytest_tutorial/my_tests/test_suite2_with_class.py
@@ -0,0 +1,13 @@
+
+
+
+class TestChecout(object):
+
+    def test_checkout_as_guest(self):
+        print("Checkout as guest")
+        print("Class: 11111111")
+
+
+    def test_checkout_with_existing_user(self):
+        print("Checkout with existing user")
+        print("Class: 2222222")
---
+++ pytest_tutorial/my_tests/test_suite_1.py
@@ -0,0 +1,12 @@
+
+
+
+def test_login_page_valid_user():
+    print("Login with valid user")
+    print("Function: aaaaaaaa")
+
+
+def test_login_page_wrong_password():
+    print("Login with wrong password")
+    print("Function: bbbbbbbb")
+    # assert 1==2, 'One is not two'
